236736103d6d1acb0990f20360fd27be8a6a5766
Lars Vilhuber <lars@vilhuber.com>
2025-01-22 12:51:50 -0500

    Switching to quarto/revealjs

--- _build.sh
+++ _build.sh
@@ -1,10 +1,29 @@
-#!/bin/sh
+#!/bin/bash
 
-set -ev
+VERSION=4.4.2
+TAG=2025-01-22
+MYHUBID=larsvilhuber
+MYNAME=replicability-training-presentation
+MYIMG=$MYHUBID/${MYNAME}:${TAG}
+
+docker=$(which docker1 2>/dev/null)
+[[ -z $docker ]] && docker=$(which docker)
 
 
-cd $(dirname $0)
+if [[ "$docker" == "/programs/docker/bin/docker1" ]]
+then
+  # we need to copy the Stata license
+  BIOHPCWD=/workdir/$(whoami)
+  [[ -d $BIOHPCWD ]] || mkdir $BIOHPCWD
+  chmod a+rwx $BIOHPCWD
+  chmod a+rw $BIOHPCWD/*
+fi
 
-# build the handbook
-Rscript _build.R
+$docker run -it --rm \
+  -v "$(pwd)/":/project \
+  -w /project \
+  $MYIMG quarto render index.qmd
+
+
+
 
