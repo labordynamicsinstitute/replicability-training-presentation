ed0e5f7e5d806b080361f497376e2c0680d89d9a
Lars Vilhuber <lars.vilhuber@cornell.edu>
2025-04-12 08:29:11 -0400

    adding a script to render the full site

---
+++ build_quarto.sh
@@ -0,0 +1,8 @@
+quarto render index.qmd --output-dir _html
+quarto render part1a.qmd --output-dir _html
+quarto render part1b.qmd --output-dir _html
+quarto render part2.qmd --output-dir _html
+quarto render part4.qmd --output-dir _html
+quarto render part5.qmd --output-dir _html
+quarto render discussion.qmd --output-dir _html
+quarto render fulltoc.qmd --output-dir _html
